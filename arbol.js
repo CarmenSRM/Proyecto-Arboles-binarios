bf8fe5d3d27ee8b1f56d99a7069e6bc104379d7b
CarmenSRM <cramos15@ucol.mx>
2022-11-21 23:32:02 -0600

    Proyecto árboles binarios

---
+++ arbol.js
@@ -0,0 +1,16 @@
+class Arbol{
+    constructor(){
+        this.raiz = null;
+    }
+
+    crear(lista){
+    }
+
+    obtenerPreOrder(){}
+
+    obtenerPosOrder(){}
+
+    resolverPreOrder(preOrder){}
+
+    resolverPosOrder(posOrder){}
+}
---
+++ nodos.js
@@ -0,0 +1,9 @@
+class Nodo{
+    constructor(valor){
+        this.valor = valor;
+        this.sig = null;
+        this.ant = null;
+        this.izq = null;
+        this.der = null;        
+    }
+}
