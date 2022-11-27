ea7f793d108879c6e23d75a8935b44e437e091f0
CarmenSRM <cramos15@ucol.mx>
2022-11-27 16:42:04 -0600

    Resolver PreOrden y PosOrden

--- main.js
+++ main.js
@@ -3,36 +3,18 @@
     3.- Agregar a una lista.                 x
     4.- Trasformar la lista a un árbol.          x
     5.- Crear preOrden.                      x
-    6.- Crear PosOrder.                      x
+    6.- Crear posOrden.                      x
     7.- Resolver el preOrden.                x
-    8.- Resolver el PosOrder.                    x
+    8.- Resolver el posOrden.                x
 */
 
 import Lista from './lista.js';
 import Nodo from './nodos.js'
-import Arbol from './arbol.js'
 import ArbolBinario from './arbol.js';
 
 let lista =  new Lista();
 /*
 let arbol = new ArbolBinario();
-let nodo = new Nodo(20);
-arbol.crear(nodo);
-
-nodo = new Nodo(15);
-arbol.crear(nodo);
-
-nodo = new Nodo(10);
-arbol.crear(nodo);
-
-nodo = new Nodo(30);
-arbol.crear(nodo);
-
-nodo = new Nodo(45);
-arbol.crear(nodo);
-
-nodo = new Nodo(25);
-arbol.crear(nodo);
 
 console.log('PreOrden');
 console.log(arbol.obtenerPreOrden());
@@ -42,10 +24,10 @@ console.log(arbol.obtenerPosOrden());
 
 let ecuacion = '4+2*5-6*4/2';
 
-console.log(ecuacion);
+//console.log(ecuacion);
 
 ecuacion = separarValores(ecuacion);
-console.log(ecuacion);
+//console.log(ecuacion);
 
 //console.log(incluirEnLista(ecuacion));
 //console.log(lista.mostrarLista());
@@ -79,26 +61,25 @@ function incluirEnLista(expresion){
     return lista.mostrarLista();
 }
 */
-let preOrden = '-+4*25/*642';
-console.log(preOrden);
+let preOrden = '++-+4*3269/*369'; 
+//console.log(preOrden);
 
-let posOrder = '425*+64*2/-';
-console.log(posOrder);
+let posOrden =  '83+62*-35*-43*2*8/+';
+//console.log(posOrden);
 
+console.log(resolverPreOrden(preOrden));
+console.log(resolverPosOrden(posOrden));
 
-
-console.log(`Resultado del preOrden =  ${resolverpreOrden(preOrden)}`);
-//console.log(resolverPosOrder(posOrder));
-
-function resolverpreOrden(preOrden){
+//7
+function resolverPreOrden(preOrden){
     preOrden = separarValores(preOrden);
     let solucion = [];
-    do{
-        if(Number.isInteger(preOrden[preOrden.length-1]) === true ){
-            solucion.push(preOrden[preOrden.length-1]);
+    for(let i = preOrden.length-1; i >= 0; i--){
+        if(Number.isInteger(preOrden[i]) === true ){
+            solucion.push(preOrden[i]);
             preOrden.pop();
         }else{
-            let operacion = preOrden[preOrden.length-1];
+            let operacion = preOrden[i];
             preOrden.pop();
             let num1 = solucion[solucion.length-1];
             solucion.pop();
@@ -115,9 +96,36 @@ function resolverpreOrden(preOrden){
                 operacion = num1 / num2; 
             } 
             solucion.push(operacion);
-
         }
-    }while(preOrden.length === 0 && solucion.length === 1);
+    }
+    return solucion;
+}
 
-    return solucion[0];
+//8
+function resolverPosOrden(posOrden){
+    posOrden = separarValores(posOrden);
+    let solucion = [];
+    for(let i = 0; i < posOrden.length; i++){
+        if(Number.isInteger(posOrden[i]) === true ){
+            solucion.push(posOrden[i]);
+        }else{
+            let operacion = posOrden[i];
+            let num1 = solucion[solucion.length - 1];
+            solucion.pop();
+            let num2 = solucion[solucion.length - 1];
+            solucion.pop();
+
+            if(operacion === '+'){
+                operacion = num2 + num1; 
+            }else if(operacion === '-'){
+                operacion = num2 - num1; 
+            }else if(operacion === '*'){
+                operacion = num2 * num1; 
+            }else{
+                operacion = num2 / num1; 
+            } 
+            solucion.push(operacion);
+        }
+    }
+    return solucion;
 }
